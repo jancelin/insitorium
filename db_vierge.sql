7810b790c7939960b85cddd129aad7016e9156e3
ANCELIN Julien <julien.ancelin@inra.fr>
2022-08-17 11:26:27 +0200

    Create db_vierge.sql

---
+++ db_vierge.sql
@@ -0,0 +1,80 @@
+
+CREATE DATABASE insitorium
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+ALTER DATABASE insitorium
+    SET search_path TO '"$user", public, topology';
+-- Table: public.point
+
+-- DROP TABLE public.point;
+
+CREATE TABLE public.point
+(
+    id serial,
+    geom geometry(PointZ,4171),
+    hauteur numeric,
+    ordre integer,
+    rayon numeric,
+    texte character varying(25) COLLATE pg_catalog."default",
+    central boolean,
+    ausol boolean,
+    CONSTRAINT pk_point PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.point
+    OWNER to admin;
+
+-- Index: idx_geom
+
+-- DROP INDEX public.idx_geom;
+
+CREATE INDEX idx_geom
+    ON public.point USING gist
+    (geom gist_geometry_ops)
+    TABLESPACE pg_default;
+
+    -- Table: public.user_position
+
+    -- DROP TABLE public.user_position;
+
+    CREATE TABLE public.user_position
+    (
+        id serial,
+        user_ins character varying COLLATE pg_catalog."default",
+        dat numeric,
+        lati numeric,
+        longi numeric,
+        height numeric,
+        direct numeric,
+        CONSTRAINT id_user_insitorium PRIMARY KEY (id)
+    )
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.user_position
+        OWNER to admin;
+
+    GRANT ALL ON TABLE public.user_position TO admin;
+
+    GRANT SELECT ON TABLE public.user_position TO replicator;
+
+    -- Index: inx_user_pos
+
+    -- DROP INDEX public.inx_user_pos;
+
+    CREATE INDEX inx_user_pos
+        ON public.user_position USING btree
+        (id DESC)
+        TABLESPACE pg_default;
