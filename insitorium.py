18747f377388514b9c6c0bb2aec7ba6ae7430883
ANCELIN Julien <julien.ancelin@inra.fr>
2022-08-26 16:31:03 +0200

    comment fonctions

--- insitorium.py
+++ insitorium.py
@@ -1,4 +1,23 @@
- Location of music on your computer
+
+##------------------------------------------------------------------------------
+##------------------------------------------------------------------------------
+## Play Insitorium land sound on your smartphone with rtk location
+##------------------------------------------------------------------------------
+##------------------------------------------------------------------------------
+## Use pydroid3 on your smartphone for runnig:
+## * Pydroid 3 - IDE for Python 3:
+##      https://play.google.com/store/apps/details?id=ru.iiec.pydroid3&hl=fr&gl=US
+## * ACtivate location : Setting > system > additionnal permissions:
+##      https://play.google.com/store/apps/details?id=ru.iiec.pydroidpermissionsplugin
+## * Pydroid repository plugin:
+##      https://play.google.com/store/apps/details?id=ru.iiec.pydroid3.quickinstallrepo
+## * PIP INSTALL :
+##      * pg8000
+##      * plyer
+## Use rtk connexion for location with Lefebure and CentipedeRTK:
+##  * https://play.google.com/store/search?q=lefebure+ntrip&c=apps&hl=fr&gl=FR
+##  * https://docs.centipede.fr/docs/Rover_rtklib_android/#application-lefebure-propri%C3%A9taire
+##------------------------------------------------------------------------------
 
 import pygame
 import os
@@ -8,20 +27,24 @@ import time
 from plyer import gps
 from kivy.lang import Builder
 
+## Where sounds ?
 MUSIC_PATH = "./son/all/"
 
+## Online Database connexion
 HOST = "localhost"
 PORT = 5432
 USER = "geobeat"
 PASSWORD = "password"
 DATABASE = "insitorium"
 
+## Blank variables
 list_sound = []
 list_volume =[]
 list_play = []
 list_stop = []
 old_sound = []
 
+## Get location
 def gnss_llh(**kwargs):
     global lat
     global lon
@@ -41,7 +64,7 @@ def gnss_llh(**kwargs):
     # print(lon)
     # print(elv)
 
-
+## Get nearest geolacated sounds from online database
 def database():
     global res
     global res_pos
@@ -78,6 +101,7 @@ def database():
     print(res)
     conn.close()
 
+## Transform database values to python pygame Sound command (load, play, volume, fadeout,...)
 def query():
     global list_play
     global list_volume
@@ -109,17 +133,19 @@ def query():
     # print(list_volume)
     # print(list_play)
 
+## Running insitorium
 def run():
     old_sound = []
     old_play = []
     while True:
         try:
+            ## Start GPS & get positions
             gps.configure(on_location=gnss_llh)
             gps.start()
-            #time.sleep(1)
+            ## Get nerest sounds with
             query()
-            if list_sound: #don't start if no new data
-                #LISTEN!: Load > play > volume
+            if list_sound: ## don't start if no new data
+                ## LISTEN!: Load > play > volume or fadeout sound
                 for l_s in list_sound:
                     if l_s in old_sound:
                         print('same sound')
@@ -137,7 +163,7 @@ def run():
                             if o_p in list_play:
                                 print('tjs dans la zone')
                             else:
-                                exec(o_p[:-9]+".stop()")
+                                exec(o_p[:-9]+".fadeout(900)")
                                 print('Sortie de zone', o_p[:-9])
                 for l_v in list_volume:
                     print(l_v)
